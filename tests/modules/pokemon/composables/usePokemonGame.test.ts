2bf33a7d814a896f0a7ccd88732932ad1ed7f60c
Andres_Godinez <ing.a.godinez@gmail.com>
2025-05-11 20:24:49 -0600

    feat: enhance tests for usePokemonGame to validate game status and loading behavior

--- tests/modules/pokemon/composables/usePokemonGame.test.ts
+++ tests/modules/pokemon/composables/usePokemonGame.test.ts
@@ -1,9 +1,29 @@
 import { usePokemonGame } from '../../../../src/modules/pokemon/composables/usePokemonGame.ts';
 import { withSetup } from '../../../utils/setup-utils.ts';
+import { GameStatus } from '../../../../src/modules/pokemon/interfaces';
+import { flushPromises } from '@vue/test-utils';
 
 describe('usePokemonGame', () => {
-  test('should have the correct properties', () => {
+  test('should have the correct properties', async () => {
     const [result, app] = withSetup(usePokemonGame);
+    expect(result.gameStatus.value).toEqual(GameStatus.Playing);
+    expect(result.isLoading.value).toEqual(true);
+    expect(result.pokemonOptions.value).toEqual([]);
+    expect(result.randomPokemon.value).toBeUndefined();
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    await flushPromises();
+
+    expect(result.isLoading.value).toEqual(false);
+    expect(result.pokemonOptions.value.length).toEqual(4);
+    console.log(result.randomPokemon.value);
+
+    expect(result.randomPokemon.value).toEqual({
+      id: expect.any(Number),
+      name: expect.any(String),
+    });
+
 
   });
 });
